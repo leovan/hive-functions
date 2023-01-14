4cd97a9ff448c886e9b7366ec5886e2fe26a2249
Leo <leo-van@hotmail.com>
2023-01-14 20:44:35 +0800

    添加根据 JSON PATH 提取 JSON 中对应的值

--- docs/json-functions.md
+++ docs/json-functions.md
@@ -48,3 +48,122 @@ SELECT
   }
 }
 ```
+
+### `EXTRACT_JSON`
+
+根据 JSON PATH 提取 JSON 中对应的值。
+
+```sql
+CREATE TEMPORARY FUNCTION
+  EXTRACT_JSON
+AS
+  'me.leovan.hive.udf.json.ExtractJsonUDF';
+```
+
+#### 参数
+- JSON（必选）：待提取的 JSON
+- JSON_PATH（必选）：JSON Path 表达式
+
+#### 示例
+
+##### 示例 1
+
+```sql
+SELECT
+  EXTRACT_JSON(
+    '{"k1": "v1", "k2": 3}',
+    '$.k1'
+  )
+;
+```
+
+输出结果为：
+
+```
+"v1"
+```
+
+##### 示例 2
+
+```sql
+SELECT
+  EXTRACT_JSON(
+    '{"k1": "v1", "k2": 3}',
+    '$.k2'
+  )
+;
+```
+
+输出结果为：
+
+```
+3
+```
+
+##### 示例 3
+
+```sql
+SELECT
+  EXTRACT_JSON(
+    '{"k": ["v", 3]}',
+    '$.k[*]'
+  )
+;
+```
+
+输出结果为：
+
+```
+["v",3]
+```
+
+##### 示例 4
+
+```sql
+SELECT
+  EXTRACT_JSON(
+    '{"k": ["v", 3]}',
+    '$.k[0]'
+  )
+;
+```
+
+输出结果为：
+
+```
+"v"
+```
+
+##### 示例 5
+
+```sql
+SELECT
+  EXTRACT_JSON(
+    '{"m": {"k1": "v1", "k2": 3}}',
+    '$.m'
+  )
+;
+```
+
+输出结果为：
+
+```
+{"k1":"v1","k2":3}
+```
+
+##### 示例 6
+
+```sql
+SELECT
+  EXTRACT_JSON(
+    '{"m": {"k1": "v1", "k2": 3}}',
+    '$.m.k2'
+  )
+;
+```
+
+输出结果为：
+
+```
+3
+```
---
+++ src/main/java/me/leovan/hive/udf/json/ExtractJsonUDF.java
@@ -0,0 +1,58 @@
+package me.leovan.hive.udf.json;
+
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.spi.json.GsonJsonProvider;
+import com.jayway.jsonpath.spi.json.JsonProvider;
+import com.jayway.jsonpath.spi.mapper.GsonMappingProvider;
+import com.jayway.jsonpath.spi.mapper.MappingProvider;
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+@Description(
+        name = "EXTRACT_JSON",
+        value = "_FUNC_(JSON, JSON_PATH) - 根据 JSON PATH 提取 JSON 中对应的值"
+)
+public class ExtractJsonUDF extends UDF {
+    static {
+        Configuration.setDefaults(new Configuration.Defaults() {
+            private final JsonProvider jsonProvider = new GsonJsonProvider();
+            private final MappingProvider mappingProvider = new GsonMappingProvider();
+
+            @Override
+            public JsonProvider jsonProvider() {
+                return jsonProvider;
+            }
+
+            @Override
+            public Set<Option> options() {
+                return EnumSet.noneOf(Option.class);
+            }
+
+            @Override
+            public MappingProvider mappingProvider() {
+                return mappingProvider;
+            }
+        });
+    }
+
+    public String evaluate(String jsonStr, String jsonPath) {
+        if (jsonPath == null || "".equals(jsonPath.trim())) {
+            return null;
+        }
+
+        String extractedStr = null;
+
+        try {
+            extractedStr = JsonPath.parse(jsonStr).read(jsonPath).toString();
+        } catch (Exception e) {
+            return null;
+        }
+
+        return extractedStr;
+    }
+}
